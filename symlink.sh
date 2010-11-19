b142b0a9ab30c6775b5fb75b9a40ebe14717e2b8
Harley Pig <harleypig@gmail.com>
2010-11-19 11:00:02 -0700

    removed unused dotfiles from symlink script

--- symlink.sh
+++ symlink.sh
@@ -18,7 +18,7 @@ DOTFILE="${DOTFILE} .bashrc"
 DOTFILE="${DOTFILE} .calcurse"
 DOTFILE="${DOTFILE} .cvsrc"
 DOTFILE="${DOTFILE} .elinks"
-DOTFILE="${DOTFILE} .flexget"
+#DOTFILE="${DOTFILE} .flexget" # Not needed for work environment
 DOTFILE="${DOTFILE} .git"
 DOTFILE="${DOTFILE} .gitconfig"
 DOTFILE="${DOTFILE} .gitignore_global"
@@ -26,26 +26,12 @@ DOTFILE="${DOTFILE} .htoprc"
 DOTFILE="${DOTFILE} .mplayer"
 DOTFILE="${DOTFILE} .perldb"
 DOTFILE="${DOTFILE} .perltidyrc"
-DOTFILE="${DOTFILE} .rtorrent.rc"
+#DOTFILE="${DOTFILE} .rtorrent.rc" # Not needed for work environment
 DOTFILE="${DOTFILE} .screenrc"
 
 for i in ${DOTFILE}
 do
 
-  echo ln -s ${DOTFILE_DIR}/$i ${HOME}/$i
+  ln -s ${DOTFILE_DIR}/$i ${HOME}/$i
 
 done
-
-#ln -s ${DOTFILE_DIR}/.bash_aliases ${HOME}/.bash_aliases
-#ln -s ${DOTFILE_DIR}/.bash_completion.d ${HOME}/.bash_completion.d
-#ln -s ${DOTFILE_DIR}/.bash_logout ${HOME}/.bash_logout
-#ln -s ${DOTFILE_DIR}/.bash_profile ${HOME}/.bash_profile
-#ln -s ${DOTFILE_DIR}/.bashrc ${HOME}/.bashrc
-#ln -s ${DOTFILE_DIR}/.cvsrc ${HOME}/.cvsrc
-#ln -s ${DOTFILE_DIR}/.gitconfig ${HOME}/.gitconfig
-##ln -s ${DOTFILE_DIR}/.gitignore_global ${HOME}/.gitignore_global
-#ln -s ${DOTFILE_DIR}/.htoprc ${HOME}/.htoprc
-#ln -s ${DOTFILE_DIR}/.perldb ${HOME}/.perldb
-#ln -s ${DOTFILE_DIR}/.perltidyrc ${HOME}/.perltidyrc
-#ln -s ${DOTFILE_DIR}/.screenrc ${HOME}/.screenrc
-
