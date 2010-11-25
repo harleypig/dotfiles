86430feffc3659adf8f4d1e6ae911f9e3d5a2eec
Harley Pig <harleypig@gmail.com>
2010-11-24 19:38:34 -0700

    various changes

--- symlink.sh
+++ symlink.sh
@@ -2,36 +2,20 @@
 
 [[ -z ${HOME} ]] && $HOME='/home/harleypig'
 
-DOTFILE_DIR="${HOME}/projects/dotfiles"
+SELF=$(basename $(readlink -f $0))
+DOTFILE_DIR=$(dirname $(readlink -f $0))
 
 cd ${HOME}
 
-DOTFILE=
-DOTFILE="${DOTFILE} .aptitude"
-DOTFILE="${DOTFILE} .bash_aliases"
-DOTFILE="${DOTFILE} .bash_completion.d"
-DOTFILE="${DOTFILE} .bash_functions"
-DOTFILE="${DOTFILE} .bash_logout"
-DOTFILE="${DOTFILE} .bash_profile"
-DOTFILE="${DOTFILE} .bash_prompt"
-DOTFILE="${DOTFILE} .bashrc"
-DOTFILE="${DOTFILE} .calcurse"
-DOTFILE="${DOTFILE} .cvsrc"
-DOTFILE="${DOTFILE} .elinks"
-DOTFILE="${DOTFILE} .flexget"
-DOTFILE="${DOTFILE} .git"
-DOTFILE="${DOTFILE} .gitconfig"
-DOTFILE="${DOTFILE} .gitignore_global"
-DOTFILE="${DOTFILE} .htoprc"
-DOTFILE="${DOTFILE} .mplayer"
-DOTFILE="${DOTFILE} .perldb"
-DOTFILE="${DOTFILE} .perltidyrc"
-DOTFILE="${DOTFILE} .rtorrent.rc"
-DOTFILE="${DOTFILE} .screenrc"
+# We want to link these files
+DOTFILES=( $(ls -A ${DOTFILE_DIR}) )
 
-for i in ${DOTFILE}
+for d in "${DOTFILES[@]}"
 do
 
-  ln -s ${DOTFILE_DIR}/$i ${HOME}/$i
+  [ $d == ${SELF} ] && continue
+  [ $d == 'README' ] && continue
+
+  echo ln -s $d ${HOME}/$d
 
 done
