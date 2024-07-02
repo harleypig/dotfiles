7d4ce9047b92b37cda315bf3de8396a072cd8df2
Alan Young <harleypig@harleypig.com>
2024-07-02 11:47:50 -0600

    Redefined the `files_from_yaml` variable as a map of objects in order to modify a key value in the object.

--- terraform/variable-validation/variables.tf
+++ terraform/variable-validation/variables.tf
@@ -3,3 +3,16 @@ variable "data_folder" {
   type        = string
   default     = "myfolder"
 }
+
+# because we are modifying a key value in the object, we need to redefine the
+# entire object
+
+variable "files_from_yaml" {
+  description = "files in the module"
+  type = map(object({
+    filename             = string
+    content              = string
+    file_permission      = string
+    directory_permission = string
+  }))
+}
