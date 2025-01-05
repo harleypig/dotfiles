0e1c2a1a96dc29f2ab7d68ac01eb585c8701fb4e
Alan Young <harleypig@harleypig.com>
2025-01-05 15:23:36 -0700

    chore: Remove unused settings and add Update-All PowerShell script

---
+++ powershell/bin/Update-All.ps1
@@ -0,0 +1,8 @@
+# Modify the Get-InstalledModule line to keep the user informed.
+# * Each message should appear on the same line, with the final message "All
+#   modules updated." being the only line seen.
+
+Get-InstalledModule | ForEach-Object { Update-Module -Name $_.Name }
+
+# Update all installed scripts
+Get-InstalledScript | ForEach-Object { Update-Script -Name $_.Name }
