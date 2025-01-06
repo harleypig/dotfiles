70cfeda37a9c967c591fe62a0748f871e50c96b6
Alan Young <harleypig@harleypig.com>
2025-01-05 20:18:41 -0700

    refactor: Remove unnecessary variable assignments in loadtokens script

--- powershell/startup/000-loadtokens.ps1
+++ powershell/startup/000-loadtokens.ps1
@@ -3,19 +3,16 @@ $private_dotfiles = "$env:PROJECTS_DIR/private_dotfiles"
 #-----------------------------------------------------------------------------
 if (Test-Path "$private_dotfiles/api-key.azure") {
     $env:AZURE_DEVOPS_EXT_PAT = Get-Content -Path "$private_dotfiles/api-key.azure" -Raw
-    $AZURE_DEVOPS_EXT_PAT = $env.AZURE_DEVOPS_EXT_PAT
 }
 
 #-----------------------------------------------------------------------------
 if (Test-Path "$private_dotfiles/api-key.openai") {
     $env:OPENAI_API_KEY = Get-Content -Path "$private_dotfiles/api-key.openai" -Raw
-    $OPENAI_API_KEY = $env:OPENAI_API_KEY
 }
 
 #-----------------------------------------------------------------------------
 if (Test-Path "$private_dotfiles/api-key.linode") {
     $env:LINODE_TOKEN = Get-Content -Path "$private_dotfiles/api-key.linode" -Raw
-    $LINODE_TOKEN = $env:LINODE_TOKEN
 }
 
 #-----------------------------------------------------------------------------
@@ -24,12 +21,10 @@ if (Test-Path "$private_dotfiles/api-key.linode") {
 if (Get-Command vault -ErrorAction SilentlyContinue) {
     if (Test-Path "$private_dotfiles/vault.addr") {
         $env:VAULT_ADDR = Get-Content -Path "$private_dotfiles/vault.addr" -Raw
-        $VAULT_ADDR = $env:VAULT_ADDR
     }
 
     if (Test-Path "$private_dotfiles/pass.ldap") {
         $env:LDAP_PASS = Get-Content -Path "$private_dotfiles/pass.ldap" -Raw
-        $LDAP_PASS = $env:LDAP_PASS
     }
 }
 
