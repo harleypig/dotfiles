4668965581d8f5ce5e5c45c709d81ffbdc6a58eb
Alan Young <harleypig@harleypig.com>
2025-01-25 18:55:27 -0700

    feat: Add AIDER_LINE_ENDINGS environment variable for line endings setting

--- powershell/startup/aider.ps1
+++ powershell/startup/aider.ps1
@@ -1,15 +1,13 @@
 # OPENAI_API_KEY is loaded in 000-loadtokens
 
-#if ($null -ne (Get-Variable -Name $env:AIDER_OPENAI_API_KEY -ErrorAction SilentlyContinue)) {
-if ($null -ne $env:OPENAI_API_KEY) {
-    if (Get-Command aider -ErrorAction SilentlyContinue) {
-        #Set-Variable -Name AIDER_
-        $env:AIDER_DARK_MODE = "true"
-        $env:AIDER_EDITOR = "code --wait"
-        $env:AIDER_GITIGNORE = "false"
-        $env:AIDER_MAP_TOKENS = 2048
-        $env:AIDER_MAX_CHAT_HISTORY_TOKENS=2048
-        $env:AIDER_VIM = "true"
-        $env:AIDER_WATCH_FILES = "true"
-    }
+if (Get-Command aider -ErrorAction SilentlyContinue) {
+    #Set-Variable -Name AIDER_
+    $env:AIDER_DARK_MODE = "true"
+    $env:AIDER_EDITOR = "code --wait"
+    $env:AIDER_GITIGNORE = "false"
+    $env:AIDER_LINE_ENDINGS='lf'
+    $env:AIDER_MAP_TOKENS = 2048
+    $env:AIDER_MAX_CHAT_HISTORY_TOKENS=2048
+    $env:AIDER_VIM = "true"
+    $env:AIDER_WATCH_FILES = "true"
 }
